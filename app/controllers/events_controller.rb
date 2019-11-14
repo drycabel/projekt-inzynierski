c6fab93db928d9b79f7d128b248d6995ed10f538
Dawid Rycąbel <rycabel.dawid@gmail.com>
2019-11-14 13:30:49 +0100

    Join Events stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,17 @@
 class EventsController < ApplicationController
 
-    def index 
+    def index
         @events = Event.all
     end
 
-    def new 
-        @event = Event.new
+    def new
+        @form = EventCreatorForm.new
     end
 
-    def create 
-        # stworzyc membership dla ownera i przypiac ownera / transakcja - form object
-        # binding.pry
-        @event = Event.new(event_params)
-        if @event.save
-            redirect_to events_path, notice: "Event created successfully"            
+    def create
+        @form = EventCreatorForm.new(event_params.merge(current_user: current_user))
+        if @form.save
+            redirect_to events_path, notice: "Event created successfully"
         else
             render :new
         end
@@ -39,19 +37,16 @@ class EventsController < ApplicationController
     def confirm_destroy
         @event = Event.find(params[:id])
     end
-    
+
     def destroy
         @event = Event.find(params[:id])
         @event.destroy
         redirect_to events_path, notice: "Event destroyed successfully"
     end
 
-
-
-
     private
 
     def event_params
-        params.require(:event).permit(:title, :description)
+        params.permit(:title, :description)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,9 @@ class Event < ApplicationRecord
     has_many :memberships
     belongs_to :owner, class_name: "User", foreign_key: :owner_id
 
-    validates :title, presence: true
+    #walidacja na poziomie bazy danych
+    # validates :title, presence: true
+    def can_i_join?(user)
+        user != owner && !memberships.find_by(user: user).present?
+    end
 end
