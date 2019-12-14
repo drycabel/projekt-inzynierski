16713514bff8ac8718ea2a53658076adaed7b1d8
Dawid Rycąbel <rycabel.dawid@gmail.com>
2019-12-14 22:07:42 +0100

    The reject invitation function has ended

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,11 +3,15 @@ class InvitationMailer < ApplicationMailer
     def call(invitation)
         @invitation = invitation.reload
         @accept_url = if @invitation.recipient_id.present?
-                        new_session_url(token: @invitation.token.value, email: @invitation.email)
+                        new_session_url(token_value: @invitation.token.value, email: @invitation.email, invitation_refused: false)
                       else
-                        new_registration_url(token: @invitation.token.value, email: @invitation.email)
+                        new_registration_url(token_value: @invitation.token.value, email: @invitation.email)
                       end
-        @refuse_url = "#"
+        @refuse_url = if @invitation.recipient_id.present?
+                        new_session_url(token_value: @invitation.token.value, email: @invitation.email, invitation_refused: true)
+                      else
+                        invitations_refuse_url(token_value: @invitation.token.value, email: @invitation.email)
+                      end
         mail(to: @invitation.email, subject: 'Event invitation')
     end
 end
