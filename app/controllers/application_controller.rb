f05653c2f74e341984bc1bd8e3496d8780d5572a
DAWID RYCĄBEL <rycabel.dawid@gmail.com>
2020-06-23 15:58:40 +0200

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user
 
-    def current_user
-        return @current_user if defined?(@current_user)
-        @current_user = User.find_by(id: session[:user_id])
+    # def current_user
+    #     return @current_user if defined?(@current_user)
+    #     @current_user = User.find_by(id: session[:user_id])
        
+    # end
+    def current_user
+        return unless session[:user_id]
+        @current_user ||= User.find(session[:user_id])
     end
 
     helper_method :current_user
