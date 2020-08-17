dd5209480271e3e33f18ee6b719f167ff43e1bb3
DAWID RYCĄBEL <rycabel.dawid@gmail.com>
2020-08-17 18:30:41 +0200

    Create new nav bar

--- app/decorator/user_decorator.rb
+++ app/decorator/user_decorator.rb
@@ -1,35 +1,39 @@
 class UserDecorator
 
-    def initialize(object)
-        @object = object
+    def initialize(user)
+        @user = user
     end
 
     def short_bio
-        @object&.short_bio || "You don't have short biography. Edit profile and write something about yourself :)"
+        @user&.short_bio || "You don't have short biography. Edit profile and write something about yourself :)"
     end
 
     def name
-        @object&.name || "Name"
+        @user&.name || "Name"
     end
 
     def surname
-        @object&.surname || "Surname"
+        @user&.surname || "Surname"
     end
 
     def birth_date
-        @object&.birth_date || "YYYY-MM-DD"
+        @user&.birth_date || "YYYY-MM-DD"
     end
 
     def age
-        if @object.birth_date.present?
+        if @user.birth_date.present?
             now = Time.zone.now.to_date
-            now.year - @object.birth_date.year - ((now.month > @object.birth_date.month || (now.month == @object.birth_date.month && now.day >= @object.birth_date.day)) ? 0 : 1)
+            now.year - @user.birth_date.year - ((now.month > @user.birth_date.month || (now.month == @user.birth_date.month && now.day >= @user.birth_date.day)) ? 0 : 1)
         else
             "X"
         end
     end
 
-    delegate :email, to: :@object
-    # delegate :email, :name, :surname, :birth_date, to: :@object
+    def short_email
+        @user&.email&.truncate(16) || "-"
+    end
+
+    delegate :email, to: :@user
+    # delegate :email, :name, :surname, :birth_date, to: :@user
 
 end
