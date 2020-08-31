75ba59c3641f9aa4f10d268cb94f1903864d60fa
DAWID RYCĄBEL <rycabel.dawid@gmail.com>
2020-08-31 12:34:51 +0200

    Adding icons in progress

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
     has_many :memberships, dependent: :destroy
     has_many :users, through: :memberships
     belongs_to :owner, class_name: "User", foreign_key: :owner_id
-
+    
     ROLES = {
         owner: "owner",
         guest: "guest",
